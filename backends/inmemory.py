2867d76a7de3e470b5db8fb0129199f35ca00159
Mårten Borgström <martenborgstrom@gmail.com>
2017-03-25 15:04:33 +0100

    Extracted in-memory backend to separate module

---
+++ backends/inmemory.py
@@ -0,0 +1,78 @@
+import itertools
+from collections import defaultdict
+
+
+class Mailbox(object):
+    def __init__(self, msgs=None):
+        if msgs:
+            self.lastId = msgs[-1]['id']
+            self.msgs = msgs
+        else:
+            self.lastId = 0
+            self.msgs = []
+
+    def post_msg(self, subject, content):
+        self.lastId += 1
+        msg = {
+            'id': self.lastId,
+            'subject': subject,
+            'content': content,
+        }
+        self.msgs.append(msg)
+        return msg
+
+
+USER_MAILBOXES = defaultdict(Mailbox)
+USER_MAILBOXES['martenbr@example.com'] = Mailbox([
+    {'id': 1, 'subject': 'Hello 1', 'content': 'First Message'},
+    {'id': 2, 'subject': 'Hello 2', 'content': 'Second Message'},
+])
+
+USER_MAILBOXES['test@example.com'] = Mailbox([
+    {'id': 1, 'subject': 'Hello', 'content': 'First Message'},
+    {'id': 2, 'subject': 'Hello', 'content': 'Second Message'},
+    {'id': 3, 'subject': 'Hello', 'content': 'Third Message'},
+])
+
+
+def get_msgs(username, older_than=None, newer_than=None, limit=None):
+    mailbox = USER_MAILBOXES.get(username, None)
+    if not mailbox:
+        return []
+    msgs_newest_first = reversed(mailbox.msgs)
+
+    if older_than is not None:
+        msgs_newest_first = itertools.dropwhile(lambda msg: msg['id'] >= older_than, msgs_newest_first)
+
+    if newer_than is not None:
+        msgs_newest_first = itertools.takewhile(lambda msg: msg['id'] > newer_than, msgs_newest_first)
+
+    if limit:
+        msgs_newest_first = itertools.islice(msgs_newest_first, limit)
+    return list(msgs_newest_first)
+
+
+def send_msg(username, subject, content):
+    mailbox = USER_MAILBOXES[username]
+    msg = mailbox.post_msg(subject, content)
+    return msg
+
+
+def delete_msg(username, msg_id):
+    mailbox = USER_MAILBOXES.get(username, None)
+    if mailbox:
+        for i, msg in enumerate(mailbox.msgs):
+            if msg['id'] == msg_id:
+                del mailbox.msgs[i]
+                break
+
+
+def delete_old_msgs(username, older_than_id):
+    mailbox = USER_MAILBOXES.get(username, None)
+    if mailbox and mailbox.msgs:
+        if mailbox.msgs[-1]['id'] < older_than_id:
+            mailbox.msgs = []
+        for i, msg in enumerate(mailbox.msgs):
+            if msg['id'] >= older_than_id:
+                mailbox.msgs = mailbox.msgs[i:]
+                break
--- mailbox_app.py
+++ mailbox_app.py
@@ -1,44 +1,11 @@
-from collections import defaultdict
-
 from flask import Flask
 from flask_restful import reqparse, Api, Resource, abort
-import itertools
+from backends import inmemory
+
+storage = inmemory
 
 app = Flask(__name__)
 api = Api(app)
-
-
-class Mailbox(object):
-    def __init__(self, msgs=None):
-        if msgs:
-            self.lastId = msgs[-1]['id']
-            self.msgs = msgs
-        else:
-            self.lastId = 0
-            self.msgs = []
-
-    def post_msg(self, subject, content):
-        self.lastId += 1
-        msg = {
-            'id': self.lastId,
-            'subject': subject,
-            'content': content,
-        }
-        self.msgs.append(msg)
-        return msg
-
-
-USER_MAILBOXES = defaultdict(Mailbox)
-USER_MAILBOXES['martenbr@example.com'] = Mailbox([
-    {'id': 1, 'subject': 'Hello 1', 'content': 'First Message'},
-    {'id': 2, 'subject': 'Hello 2', 'content': 'Second Message'},
-])
-
-USER_MAILBOXES['test@example.com'] = Mailbox([
-    {'id': 1, 'subject': 'Hello', 'content': 'First Message'},
-    {'id': 2, 'subject': 'Hello', 'content': 'Second Message'},
-    {'id': 3, 'subject': 'Hello', 'content': 'Third Message'},
-])
 
 base_parser = reqparse.RequestParser()
 base_parser.add_argument('for_user', required=True)
@@ -60,25 +27,15 @@ delete_request_parser.add_argument('older_than', type=int)
 class MsgList(Resource):
     def get(self):
         args = list_request_parser.parse_args(strict=True)
-        mailbox = USER_MAILBOXES.get(args.for_user, None)
-        if not mailbox:
-            return []
-        msgs_newest_first = reversed(mailbox.msgs)
-
-        if args.older_than is not None:
-            msgs_newest_first = itertools.dropwhile(lambda msg: msg['id'] >= args.older_than, msgs_newest_first)
-
-        if args.newer_than is not None:
-            msgs_newest_first = itertools.takewhile(lambda msg: msg['id'] > args.newer_than, msgs_newest_first)
-
-        if args.num_msgs:
-            msgs_newest_first = itertools.islice(msgs_newest_first, args.num_msgs)
-        return list(msgs_newest_first)
+        msgs = storage.get_msgs(args.for_user,
+                                older_than=args.older_than,
+                                newer_than=args.newer_than,
+                                limit=args.num_msgs)
+        return msgs
 
     def post(self):
         args = create_request_parser.parse_args(strict=True)
-        mailbox = USER_MAILBOXES[args.for_user]
-        msg = mailbox.post_msg(args.subject, args.content)
+        msg = storage.send_msg(args.for_user, args.subject, args.content)
         return msg, 201
 
     def delete(self):
@@ -88,21 +45,9 @@ class MsgList(Resource):
         if delete_by_id and delete_by_filter:
             abort(400, message="Must give only one of msg_id and older_than")
         if delete_by_id:
-            mailbox = USER_MAILBOXES.get(args.for_user, None)
-            if mailbox:
-                for i, msg in enumerate(mailbox.msgs):
-                    if msg['id'] == args.msg_id:
-                        del mailbox.msgs[i]
-                        break
+            storage.delete_msg(args.for_user, args.msg_id)
         elif delete_by_filter:
-            mailbox = USER_MAILBOXES.get(args.for_user, None)
-            if mailbox and mailbox.msgs:
-                if mailbox.msgs[-1]['id'] < args.older_than:
-                    mailbox.msgs = []
-                for i, msg in enumerate(mailbox.msgs):
-                    if msg['id'] >= args.older_than:
-                        mailbox.msgs = mailbox.msgs[i:]
-                        break
+            storage.delete_old_msgs(args.for_user, older_than_id=args.older_than)
         else:
             abort(400, message="Either of msg_id or older_than is required")
         return '', 204
